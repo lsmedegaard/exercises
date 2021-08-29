198e6bfd01d6b0cd5ea7988268b3a7de06735b59
lasse <lass948m@stud.kea.dk>
2021-08-29 13:06:10 +0200

    template literals done

---
+++ string.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const longline = "A very, very very, very, VERY! long line of text,\
+that spans across the width og the editior "
+
+const navn = "Lasse";
+const animalType = "dog";
+const animalName = "Wilma";
+
+const text = `My name is ${navn}.
+i have a ${animalType} called ${animalName}`
+
+console.log(text);
