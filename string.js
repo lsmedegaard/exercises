598dccd525ff9a4e8bdbb96940f503a34f2a13e4
lasse <lass948m@stud.kea.dk>
2021-08-29 13:24:15 +0200

    length and index

--- string.js
+++ string.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const longline = "A very, very very, very, VERY! long line of text,\
+/* const longline = "A very, very very, very, VERY! long line of text,\
 that spans across the width og the editior "
 
-const navn = "Lasse";
+const navn = "Lasse Smedegaard";
 const animalType = "dog";
 const animalName = "Wilma";
 
@@ -11,3 +11,25 @@ const text = `My name is ${navn}.
 i have a ${animalType} called ${animalName}`
 
 console.log(text);
+
+
+const len = navn.length;
+
+console.log(`${navn} is ${len} characters long`);
+
+const letter = navn[2];
+
+console.log(`the first letter of  ${navn} is ${letter} `) */
+
+const navn = "Albus Percival Wulfric Brian Dumbledore";
+const total = navn.length
+const index = navn[29];
+
+console.log(`Total number of characters is: ${total}`)
+console.log(index)
+
+
+
+//1 = 39
+//2 = b
+//3 = P
