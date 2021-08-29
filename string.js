192019b24fabc337682f8a68bc5f9f2f5fb1c803
lasse <lass948m@stud.kea.dk>
2021-08-29 14:17:11 +0200

    exercises with .substring

--- string.js
+++ string.js
@@ -21,7 +21,7 @@ const letter = navn[2];
 
 console.log(`the first letter of  ${navn} is ${letter} `) */
 
-const navn = "Albus Percival Wulfric Brian Dumbledore";
+/* const navn = "Albus Percival Wulfric Brian Dumbledore";
 const total = navn.length
 const index = navn[29];
 
@@ -33,3 +33,36 @@ console.log(index)
 //1 = 39
 //2 = b
 //3 = P
+//4 = 29
+//5 = 38
+
+
+const str1 = "  There is    space here \n  "
+const str2 = str1.trim();
+console.log(str2);
+console.log(`_${str2}_`);
+
+const fullName ="Lasse Mark Smedegaard"
+const firstName = fullName.substring(0, 6);
+const lastName = fullName.substring(11);
+
+console.log(`_${firstName}_`)
+console.log(`last name is _${lastName}_`); */
+
+
+const navn = "Albus Percival Wulfric Brian Dumbledore";
+const firstName = navn.substring(0, 5);
+const lastName = navn.substring(29);
+const middleName = navn.substring(14, 23);
+const randomCharactersInNavn = navn.substring(25, 28);
+
+
+console.log(firstName);
+
+// 1 = (0, 5)
+// 2 = (29)
+// 3 = (15, 22)
+// 4 = (14, 23)
+// 5 = (25, 28)
+// 6 = (2, 5)
+
