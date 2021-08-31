5b6da0465629cb79f71508f16fd223e81fb1d40e
lasse <lass948m@stud.kea.dk>
2021-08-31 14:29:49 +0200

    last part of capitalization

--- capitalization3.js
+++ capitalization3.js
@@ -1,14 +1,18 @@
-const navn = "lasse Mark Smedegaard"
+/* const navn = "frederik"
 
 
-function resultat () {
-    const findT = navn.substring(2, 3).toUpperCase("");
-    const foersteDel = navn.substring(0, 2).toLowerCase("");
-    const andenDel = navn.substring(3, 5).toLowerCase("");
 
-    const resultatet = (`${foersteDel}${findT}${andenDel}`)
-    console.log(resultatet)
+const findMidten = navn.substring(2, 3).toUpperCase("");
+const foersteDel = navn.substring(0, 2).toLowerCase("");
+const andenDel = navn.substring(3).toLowerCase("");
 
-}
+console.log(`${navn} bliver til ${foersteDel}${findMidten}${andenDel}`)
 
-resultat(navn)
+ */
+
+const navn = "lasse"
+
+const firstLetter = navn.substring(0,1).toUpperCase("");
+const lastSpace = navn.substring(1).toLowerCase("");
+
+console.log(`${navn} bliver til ${firstLetter}${lastSpace}`)
