f5624a042f393dfc0a2ccfb22770554d77bd0709
Lasse <lass948m@stud.kea.dk>
2021-09-29 14:51:13 +0200

    forskellige øvelser med svg

--- capitalization3.js
+++ capitalization3.js
@@ -1,42 +1,87 @@
-/* const navn = "frederik"
+// /* const navn = "frederik"
 
 
 
-const findMidten = navn.substring(2, 3).toUpperCase("");
-const foersteDel = navn.substring(0, 2).toLowerCase("");
-const andenDel = navn.substring(3).toLowerCase("");
+// const findMidten = navn.substring(2, 3).toUpperCase("");
+// const foersteDel = navn.substring(0, 2).toLowerCase("");
+// const andenDel = navn.substring(3).toLowerCase("");
 
-console.log(`${navn} bliver til ${foersteDel}${findMidten}${andenDel}`)
+// console.log(`${navn} bliver til ${foersteDel}${findMidten}${andenDel}`)
 
- */
+//  */
 
-/* const navn = "lasse"
+// /* const navn = "lasse"
 
-const firstLetter = navn.substring(0,1).toUpperCase("");
-const lastSpace = navn.substring(1).toLowerCase("");
+// const firstLetter = navn.substring(0,1).toUpperCase("");
+// const lastSpace = navn.substring(1).toLowerCase("");
 
-console.log(`${navn} bliver til ${firstLetter}${lastSpace}`)
+// console.log(`${navn} bliver til ${firstLetter}${lastSpace}`)
 
- */
+//  */
+
+
+// function capitalize(str) {
+
+
+//     const firstLetter = str.substring(0,1).toUpperCase("");
+//     const rest = str.substring(1, str.indexOf(" ")).toLowerCase("");
+
+//     const secondFirstChar = str.substring(str.indexOf(" ")+1, str.indexOf(" ")+2).toUpperCase("")
+//     const rest1 = str.substring(str.indexOf(" ")+2).toLowerCase("");
+
+//     const thirdFirstChar = str.substring(str.lastIndexOf(" ")+1).toUpperCase("");
+
+
+
+//     let result =(`${firstLetter}${rest} ${secondFirstChar}${rest1}`);
+//     console.log(rest1)
+//     return result
+
+
+// }
+
+// const name = "lasse mark smedegaard"
+// const cap = capitalize(name)
+// console.log(cap)
+
+
+
+
 
 
 function capitalize(str) {
-    const firstLetter = str.substring(0,1).toUpperCase("");
-    const lastSpace = str.substring(1).toLowerCase("");
-
-    let result =(`${firstLetter}${lastSpace}`);
-    
-    return result
-
-
+  str = str.substring(0, 1).toLocaleUpperCase() + str.substring(1).toLocaleLowerCase();
+  return str;
 }
 
-const name = "lasse"
-const cap = capitalize(name)
+function getNameParts(fullName) {
+  if (fullName.split(" ").length > 2) {
+    console.log("there is a middle name");
+    firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
+    middleName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" ")).trim();
+    lastName = fullName.slice(fullName.lastIndexOf(" "), fullName.length).trim();
+  }
+  if (fullName.split(" ").length === 2) {
+    console.log("no middle name");
+    middleName = null;
+    firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
+    lastName = fullName.slice(fullName.indexOf(" "), fullName.length).trim();
+  }
+
+  firstName = capitalize(firstName);
+  middleName = capitalize(middleName);
+  lastName = capitalize(lastName);
+
+  let sortedFullname = { firstName, middleName, lastName };
+  /* console.log(first name: ${firstName}\nmiddle name: ${middleName}\nlast name : ${lastName});
+   /
+  / console.log({ firstName, middleName, lastName }); */
+  return sortedFullname;
+}
+
+
+let navn = "tony harakat montana"
+let cap = getNameParts(navn)
 console.log(cap)
-
-
-
-
 
 
--- nameparts.js
+++ nameparts.js
@@ -13,24 +13,27 @@ console.log(lastName)
 console.log(`the first name is "${firstName}" and the middlename is "${middleName}" and the last name is "${lastName}"`)
   */
 
+
 function capitalize(str) {
-    const firstSpace = str.indexOf(" ")+1
+        str = str.substring(0, 1).toLocaleUpperCase() + str.substring(1).toLocaleLowerCase();
+        return str;
+ }
 
-/*     let spaceCheck = str.substring(str.indexOf(" ")+1, str.indexOf(" ")+2, str.lastIndexOf(" ")+3).toUpperCase();
- */
-    const firstLetter = str.substring(0,1).toUpperCase("");
-    const secondLetter = str.substring(str.indexOf(" ")+1, str.indexOf(" ")+2)
+//     const firstS = str.indexOf(" ")
+//     const lastSpace = str.lastIndexOf(" ")
+
+
+//  */
+//     const firstLetter = str.substring(str.indexOf(" ")+1).toUpperCase("");
+
+//     // const secondLetter = str.substring(str.indexOf(" ")+1, str.indexOf(" ")+2)
    
-    let result = (`${firstLetter}${secondLetter}`);
-    console.log(secondLetter)
-    
-    return result
 
 
-}
-let name = "lasse mark smedegaard"
-const cap = capitalize(name);
-console.log(cap)
+// }
+// let name = "lasse mark smedegaard"
+// const cap = capitalize(name);
+// console.log(cap)
 
 
 
@@ -38,28 +41,29 @@ console.log(cap)
 // function til at få forskellige dele af navnet
 
 function getNameParts(fullName) {
-    const firstSpace = fullName.indexOf(" ");
-    const lastSpace = fullName.lastIndexOf(" ");
-
-    const firstName = fullName.substring(0, firstSpace).trim();
-    const middleName = (fullName.substring(firstSpace, lastSpace).trim());
-    const lastName = fullName.substring(lastSpace).trim();
-
-    if (middleName === "") {
-        let result = (`${firstName} undefined ${lastName}`)
-        return result
-    } else {
-        let result = (`${firstName} ${middleName} ${lastName}`)
-        return result
+    if (fullName.split(" ").length > 2) {
+        firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
+        middleName = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" ")).trim();
+        lastName = fullName.slice(fullName.lastIndexOf(" "), fullName.length).trim();
+    } 
+    if (fullName.split(" ").length === 2) {
+        middleName = null
+        firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
+        lastName = fullName.slice(fullName.lastIndexOf(" "), fullName.length).trim();
     }
-    
-    
+    firstName = capitalize(firstName);
+    middleName = capitalize(middleName);
+    lastName = capitalize(lastName);
+
+    let cleanName = { firstName, middleName, lastName };
+
+    return cleanName;
 
 }
 
-/* const fuldenavn = "lasse mark smedegaard"
-const gNameParts = getNameParts(fuldenavn)
-console.log(gNameParts) */
+const rString = "lasse mark smedegaard"
+const gNameParts = getNameParts(rString)
+console.log(gNameParts)
 
 
 
